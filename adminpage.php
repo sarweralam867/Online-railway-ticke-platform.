f17cfc586b124d2c327e5638e8e493c67e43357d
DoubleDips <abrar.ahsan.efaz@g.bracu.ac.bd>
2023-08-07 21:21:48 +0600

    Create adminpage.php
    okay

---
+++ adminpage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+    <title>Bangladesh Railway</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.min.css">
+    <style>
+    body{
+        background: url("Bangladesh-Railway.jpg");
+        background-repeat: no-repeat;
+        background-size: 1537px 754px;
+        background-position: fixed;
+    }
+	.p1 {
+  	font-family: "Times New Roman", Times, serif;
+	}
+	</style>
+</head>
+<body> 
+	<div style = "position:absolute; right:180px; top:5px;">
+    <div class="site-logo">
+        <div class="header-logo-img">
+            <img src="https://eticket.railway.gov.bd/v2/assets/img/home/bangladesh-railway.png" width="150" alt="Bangladesh Railway Logo"> 
+        </div>
+
+    </div>
+    <h1> <span style="color:#000000" class="p1"> Bangladesh Railway </span> </h1>
+    <h4> <span style="color:#000000" class="p1"> নিরাপদ . আরামদায়ক . সাশ্রয়ী </span></h4>
+    <h1> <span style="color:#1e0f3b" class="p1"> Welcome to admin panel </span></h1>
+
+	<form action="passengerlist.php" method="post">
+    <h2> <span style="color:#1e0f3b" class="p1"> Passenger List </span></h2>
+        <input type="submit" name="goto_another_page" value="Click Here">
+    </form>
+</body>
+</html>
